a3876167fa922ec967e91176de5b7fa6e9985a9a
douglas <alaraujo.douglas@gmail.com>
2017-06-07 21:20:51 -0300

    Transferindo dados com result no satrt activity

--- app/src/main/java/douglas/android/com/demonintent/LoginActivity.java
+++ app/src/main/java/douglas/android/com/demonintent/LoginActivity.java
@@ -1,5 +1,6 @@
 package douglas.android.com.demonintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,9 +8,12 @@ import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    public static final int REQUEST_NOVO_USUARIO = 1;
     @BindView(R.id.etLogin)
     EditText etLogin;
 
@@ -24,4 +28,21 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.tvNovoUsuario)
+    public void novoUsuarioClick() {
+        Intent novoUsuario = new Intent(this, NovoUsuarioActivity.class);
+        startActivityForResult(novoUsuario, REQUEST_NOVO_USUARIO);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_NOVO_USUARIO:
+                    etLogin.setText(data.getStringExtra("USERNAME"));
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
--- app/src/main/java/douglas/android/com/demonintent/NovoUsuarioActivity.java
+++ app/src/main/java/douglas/android/com/demonintent/NovoUsuarioActivity.java
@@ -1,16 +1,39 @@
 package douglas.android.com.demonintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class NovoUsuarioActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.etUser)
+    EditText etUser;
+    @BindView(R.id.etNome)
+    EditText etNome;
+    @BindView(R.id.etPass)
+    EditText etPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_usuario);
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.btnCriar)
+    public void criar() {
+        Intent i = new Intent();
+        i.putExtra("USERNAME", etUser.getText().toString());
+        setResult(RESULT_OK, i);
+        finish();
+    }
 
 
 }
